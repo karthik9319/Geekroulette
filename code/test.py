27ceb7dca6133f99274594a5538d3c36b06f560a
Karthik <karthik.9319@gmail.com>
2020-08-30 19:50:40 +0530

    class removed

--- code/test.py
+++ code/test.py
@@ -2,26 +2,33 @@ from random import SystemRandom
 from variables import dummy_names
 cryptogen = SystemRandom()
 
-class Player():
+# class Player():
     
-    def __init__(self,name, status, prime):
-        self.status = status
-        self.prime = prime
-        self.name = name
-     
-    
-    def odd_even_check(self): 
-        if win_num % 2 == 0:
-            return True
-        else:
-            return False
+    # def __init__(self,name, guess_number, status, prime):
+    #     self.name = name
+    #     self.guess_number = guess_number
+    #     self.status = status
+    #     self.prime = prime
         
-    def prime_no_prime(self):
-        for i in range(2, win_num):
-            if i % 2 == 0:
-                return False
-            else:
-                return True
+     
+    # def win(self):
+    #     if guess_number == win_num:
+    #         return 1
+    #     else:
+    #         return 0
+    
+    # def odd_even_check(self): 
+    #     if win_num % 2 == 0:
+    #         return False
+    #     else:
+    #         return True
+        
+    # def prime_no_prime(self):
+    #     for i in range(2, win_num):
+    #         if i % 2 == 0:
+    #             return False
+    #         else:
+                # return True
     
  
 def game_logic():
@@ -33,12 +40,62 @@ def game_logic():
     win_num = big_pot.index(min(big_pot))
     return win_num  
 
-win_num = game_logic()
+win_num = 2
 no_of_players = int(input("No of players (max 5): "))
-# p = Player()
-# print(p.odd_even_check())
-# print(p.prime_no_prime())
 
+
+def odd_check(): 
+    if win_num % 2 == 0:
+        return False
+    else:
+        return True
+
+
+def prime_no_prime():
+    for i in range(2, win_num):
+        if i % 2 == 0:
+            return False
+        else:
+            return True
+
+def score_check(guess_number, status, prime):
+    
+    def test1(guess_number):
+        # print(guess_number)
+        # print(win_num)
+        if guess_number == win_num:
+            print("here")
+            return 5
+        else:
+            return 0
+        
+        
+    def test2(status):
+        status = status.capitalize()
+        orig_status = odd_check()
+        # print(orig_status)
+        # print(status)
+        if status != orig_status:
+            return 0
+        else:
+            return 1
+        
+        
+    def test3(prime):
+        prime = prime.capitalize()
+        orig_prime = prime_no_prime()
+        # print(orig_prime)
+        if prime != orig_prime:
+            return 0
+        else:
+            return 1
+    
+    x,y,z = test1(guess_number), test2(status), test3(prime)
+    print(x,y,z)
+    return x, y, z
+    
+
+    
 
 # to store player data and input player information
 players = []
@@ -46,15 +103,20 @@ for i in range(no_of_players):
     print("player {}: ".format(i+1), dummy_names[i])
     overwrite = input("overwrite (y or n)? : ")
     if overwrite == 'y':
-        player = Player( input("Player {}: ".format(i+1)), input("Odd (True or False): "), input("Prime (True or False): "))
-        players.append([player.name, player.status, player.prime])
+        player, guess_number, status, prime = input("Player {}: ".format(i+1)), input("Guess number (0-9): "), input("Odd (True or False): "), input("Prime (True or False): ")
+        # player = Player( input("Player {}: ".format(i+1)), input("Guess number (0-9): "), input("Odd (True or False): "), input("Prime (True or False): "))
+        win_status, status, prime = score_check(guess_number, status, prime)
+        players.append([player, win_status, status, prime])
         
     else:
-        player = Player( dummy_names[i], input("Odd (True or False): "), input("Prime (True or False): "))
-        players.append([player.name, player.status, player.prime])
+        player, guess_number, status, prime = dummy_names[i], input("Guess number (0-9): "), input("Odd (True or False): "), input("Prime (True or False): ")
+        # player = Player( dummy_names[i], input("Guess number (0-9): "), input("Odd (True or False): "), input("Prime (True or False): "))
+        win_status, status, prime = score_check(guess_number, status, prime)
+        players.append([player, win_status, status, prime])
 
 
 print(players)
+print(win_num)
 # print(Player.game_logic(no_of_players))
 # def game_logic():
 #     big_pot = []
