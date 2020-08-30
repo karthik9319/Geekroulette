d337eeb75d8de6e42bf8e4de12b78104496142a7
Karthik <karthik.9319@gmail.com>
2020-08-30 14:21:40 +0530

    class added

--- code/test.py
+++ code/test.py
@@ -1,34 +1,60 @@
 from random import SystemRandom
 from variables import dummy_names
-
 cryptogen = SystemRandom()
 
 class Player():
     
-    # print("No of players: ")
-    
-    def __init__(self, name):
+    def __init__(self, status, prime, name = None):
+        self.status = status
+        self.prime = prime
         self.name = name
+     
+    def game_logic(self):
+        big_pot = []
+        for i in range(10):
+            elem = [cryptogen.randrange(9) for i in range(9)]
+            sum_elem = sum(elem)
+            big_pot.append(sum_elem)
+        win_num = big_pot.index(min(big_pot))
+        return win_num
+    
+    def odd_even_check(self): 
+        if win_num % 2 == 0:
+            return True
+        else:
+            return False
+        
+    def prime_no_prime(self):
+        for i in range(2, win_num):
+            if i % 2 == 0:
+                return False
+            else:
+                return True
     
     
-
 no_of_players = int(input("No of players (max 5): "))
-
+# player = Player(input("Player: "), input("Odd or even: "))
+# print(player.name)
+# print(player.status)
 players = []
 for i in range(no_of_players):
     print("player {}: ".format(i+1), dummy_names[i])
     overwrite = input("overwrite (y or n)? : ")
     if overwrite == 'y':
-        player = input("player {}: ".format(i+1))
-        player = Player(player)
-        players.append(player.name)
+        player = Player(input("Odd or even: "), input("Prime or not prime: "), input("Player {}: ".format(i+1)))
+        # player = input("player {}: ".format(i+1))
+        # player = Player(player, )
+        players.append([player.name, player.status, player.prime])
         
     else:
-        players.append(dummy_names[i])
+        player = Player(input("Odd or even: "), input("Prime or not prime: "), dummy_names[i])
+        players.append([player.name, player.status, player.prime])
 
+# print(player.name)
+# print(player.status)
+# print(player.prime) 
 print(players)
-        
-        
+print(Player.game_logic(self))
 # def game_logic():
 #     big_pot = []
 #     for i in range(10):
@@ -38,12 +64,24 @@ print(players)
 #     win_num = big_pot.index(min(big_pot))
 #     return win_num
 
-# def odd_even_check(win_num): 
+# def odd_even_check(win_num: int) -> bool: 
 #     if win_num % 2 == 0:
 #         return True
 #     else:
 #         return False
+    
 
+# def prime_no_prime(win_num: int) -> bool:
+#     for i in range(2, win_num):
+#         if i % 2 == 0:
+#             return False
+#         else:
+#             return True
+ 
+# win_num = game_logic() 
+# print(win_num)
+# x = prime_no_prime(win_num) 
+# print(x)      
 # def win_check():
 #     if win_num in player_bets:
 #         return f" Winner is Player_{player_bets.index(win_num)+1}"
