f9fdfffbabf8b35ab8241bbc5746402d957f6115
Karthik <karthik.9319@gmail.com>
2020-08-31 23:24:40 +0530

    added red black and dozen second third function:

--- code/main.py
+++ code/main.py
@@ -5,15 +5,19 @@ from variables import dummy_names
 cryptogen = SystemRandom()
 
 
+
+
+print("======================  Game Rules  ==================================")
+
 def game_logic():
     big_pot = []
-    for i in range(9):
+    for i in range(36):
         elem = [cryptogen.randrange(9) for i in range(9)]
         # print(elem)
         sum_elem = sum(elem)
         big_pot.append(sum_elem)
     win_num = big_pot.index(min(big_pot))
-    return win_num
+    return win_num+1
 
 
 win_num = game_logic()
@@ -41,8 +45,7 @@ def prime_no_prime():
 
 
 def red_black():
-    var = cryptogen.randrange(2)
-    if var == 0:
+    if win_num in [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36]:
         h = "red"
     else:
         h = "black"
@@ -56,7 +59,18 @@ def num_stat():
         h = "high"
     return h
 
-def score_check(guess_number, status, prime):
+
+def num_range():
+    if win_num in range(1, 13):
+        h = "dozen"
+    elif win_num in range(13, 25):
+        h = "second"
+    elif win_num in range(25, 37):
+        h = "third"
+    return h
+
+def score_check(guess_number, choice, mode, status, prime):
+    
     def test1(guess_number, win_num):
         if guess_number == win_num:
             h = 5
@@ -80,37 +94,42 @@ def score_check(guess_number, status, prime):
         else:
             return 1
 
-# def test4(choice):
-#     orig_choice = red_black()
-#     if choice != orig_choice:
-#         return 0
-#     else:
-#         return 1
+    def test4(choice):
+        orig_choice = red_black()
+        if choice != orig_choice:
+            return 0
+        else:
+            return 1
 
-# def test5(option):
-    i
+    def test5(option):
+        orig_option = num_stat()
+        if option != orig_option:
+            return 0
+        else:
+            return 1
     
+    win_status = test1(guess_number, win_num)
+    status = test2(status)
+    prime = test3(prime)
+    choice = test4(choice)
+    mode = test5(mode)
+    
+    return win_status, status, prime, choice, mode
 
 
-    x = test1(guess_number, win_num)
-    y = test2(status)
-    z = test3(prime)
-    return x, y, z
 
-
-
-def winner(players):
-    for i in range(len(players)):
-        x = players[i]
-        if x[1] == 5:
-            print("Guessed right: ", x[0])
-            print("Roulette Number: ", win_num)
-        if x[2] == 1:
-            print("Lucky you!! ", x[0])
-            print("Roulette Number: ", win_num)
-        if x[3] == 1:
-            print("Awesome Guess: ", x[0])
-            print("Roulette Number: ", win_num)
+# def winner(players):
+#     for i in range(len(players)):
+#         x = players[i]
+#         if x[1] == 5:
+#             print("Guessed right: ", x[0])
+#             print("Roulette Number: ", win_num)
+#         if x[2] == 1:
+#             print("Lucky you!! ", x[0])
+#             print("Roulette Number: ", win_num)
+#         if x[3] == 1:
+#             print("Awesome Guess: ", x[0])
+#             print("Roulette Number: ", win_num)
             
 
 
@@ -120,26 +139,31 @@ for i in range(no_of_players):
     print("player {}: ".format(i + 1), dummy_names[i])
     overwrite = input("overwrite (y or n)? : ")
     if overwrite == "y":
-        player, guess_number, status, prime = (
+        player, guess_number, choice, mode, status, prime = (
             input("Player {}: ".format(i + 1)),
-            int(input("Guess number (0-9): ")),
+            int(input("Guess number (1-36): ")),
+            input("Red or Black: "),
+            input("Dozen(1-12) \n Second(13-24) \n Third(25-36): "),
             input("Odd (True or False): "),
             input("Prime (True or False): "),
         )
-        win_status, status, prime = score_check(guess_number, status, prime)
-        players.append([player, win_status, status, prime])
+        win_status, choice, mode, status, prime = score_check(guess_number, choice, mode, status, prime)
+        players.append([player, win_status, choice, mode, status, prime])
 
     else:
-        player, guess_number, status, prime = (
+        player, guess_number, choice, mode, status, prime = (
             dummy_names[i],
-            int(input("Guess number (0-9): ")),
+            int(input("Guess number (1-36): ")),
+            input("Red or Black: "),
+            input("Dozen(1-12) \nSecond(13-24) \nThird(25-36): "),
             input("Odd (True or False): "),
             input("Prime (True or False): "),
         )
-        win_status, status, prime = score_check(guess_number, status, prime)
-        players.append([player, win_status, status, prime])
+        win_status, choice, mode, status, prime = score_check(guess_number, choice, mode, status, prime)
+        players.append([player, win_status, choice, mode, status, prime])
 
 
+print(players)
 
-winner(players)
+# winner(players)
 
